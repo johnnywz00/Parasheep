c4eb5e46228ec31c38e88a87a74a1975a1c910ba
John Ziegler <johnnywz00@yahoo.com>
2026-02-13 23:42:42 -0600

    Fixing mouseMove in event loop

--- src/sfmlTguiApp.cpp
+++ src/sfmlTguiApp.cpp
@@ -72,6 +72,8 @@ void FullscreenOnlyApp::update ()
                 break;
 				
 			case Event::MouseMoved:
+				adj = window.mapPixelToCoords(vecI(event.mouseMove.x,
+												   event.mouseMove.y));
 				state.mouseVec = toVecI(adj);
 				break;
 				
